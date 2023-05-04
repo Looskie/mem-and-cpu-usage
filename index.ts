8d3efafb2a8f6f741b3c6531bbf864e3996759dd
Cody Miller <hello@looskie.com>
2023-05-04 08:30:01 -0400

    consume more memory!!!!!!!

--- index.ts
+++ index.ts
@@ -9,12 +9,21 @@ const bytesToFormattedSize = (bytes: number) => {
   return `${Math.round(bytes / Math.pow(1024, i))} ${SIZES[i]}`;
 };
 
+const array = [];
+
 setInterval(() => {
+  // assigning random stuff to simulate using memory
+  for (let i = 0; i < 5_00_000; i++) {
+    const a = i * 2;
+    array.push(a);
+  }
   printTable([
     {
+      id: process.pid,
       cpus: os.cpus().length,
       memory: bytesToFormattedSize(os.totalmem()),
       freeMemory: bytesToFormattedSize(os.freemem()),
+      memUsedByProcess: bytesToFormattedSize(process.memoryUsage().heapUsed),
       platform: os.platform(),
       release: os.release(),
       uptime: `${os.uptime()}s`,
@@ -24,4 +33,4 @@ setInterval(() => {
         .join(", "),
     },
   ]);
-}, 5_000);
+}, 1_500);
